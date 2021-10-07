cb94bffdb8bf9728f26fe7f3b672850398a2a693
facundorb <37991547+facundorb@users.noreply.github.com>
2021-10-06 21:42:30 -0300

    Agrego función guardar datos

--- docs/script.js
+++ docs/script.js
@@ -5,34 +5,53 @@ var Modal = function(otrasOpciones) {
       contenido: "",
       backdrop: true
     }
+
+    this.guardarDatos = () => {
+      this.close();
+    }
   
     if(arguments[0] && typeof arguments[0] == "object") {
       this.opciones.contenido = arguments[0].contenido || this.opciones.contenido;
       this.opciones.backdrop = arguments[0].backdrop === undefined ? true : arguments[0].backdrop;
     }
   
+
+
+  
     this.open = () => {
       if (this.modal) {
-       // return;
+        return;
       }
     
       this.modal = document.createElement('div');
       this.modal.className = 'modal';
+      this.modal.style.cssText = 'position:absolute;width:50%;height:100%;opacity:1;left:30%;background:green';
+      
+
     
-      const modalText = document.createElement('p');
-      modalText.textContent = this.opciones.contenido;
+      const modalText = document.createElement('h2');
+      modalText.textContent = 'Por favor ingresa tus datos';
+      modalText.style.cssText = 'text-align: center;'
+
+      const modaltxtNombre = document.createElement('input');
+      modaltxtNombre.className = 'txtNombre';
+      modaltxtNombre.placeholder = 'Ingrese su nombre';
+      modaltxtNombre.style.cssText = 'align-content: center;'
+      modaltxtNombre.style.cssText = 'placeholder:Ingresa tu nombre;'
     
       const modalCancelAction = document.createElement('button');
+      modalCancelAction.style.cssText = 'bg-red-500 font-bold;';
       modalCancelAction.textContent = 'Cancelar';
       modalCancelAction.className = 'btn btn--alt';
       modalCancelAction.addEventListener('click', this.close);
     
       const modalConfirmAction = document.createElement('button');
-      modalConfirmAction.textContent = 'Confirmar';
+      modalConfirmAction.textContent = 'Guardar';
       modalConfirmAction.className = 'btn';
-      modalConfirmAction.addEventListener('click', this.close);
+      modalConfirmAction.addEventListener('click', this.guardarDatos);
     
       this.modal.append(modalText);
+      this.modal.append(modaltxtNombre);
       this.modal.append(modalCancelAction);
       this.modal.append(modalConfirmAction);
     
@@ -52,14 +71,14 @@ var Modal = function(otrasOpciones) {
     this.close = () => {
       this.modal.remove();
       this.modal = null;
-  
       this.backdrop.remove();
       this.backdrop = null;
     }
+
 
     return this;
   }
   
   const button = document.querySelector('button');
-  const unModal = Modal({contenido: "SI?", backdrop: false});
+  const unModal = Modal({backdrop: true});
   button.addEventListener('click', unModal.open);
